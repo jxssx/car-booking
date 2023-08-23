d01fdae1f860a5e38f93351eb7167680e06bcdde
Daniil Druzhina <danil.druzhina@gmail.com>
2023-08-23 12:43:00 +0300

    create migration for bookings

---
+++ db/migrations/1692775466409-Bookings.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Bookings1692775466409 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Не создаем референс в carId т.к. cars в реальности были бы отдельным сервисом
+    await queryRunner.query(`
+        CREATE TABLE bookings (
+        id SERIAL PRIMARY KEY,
+        car_id INTEGER,
+        start_date TIMESTAMP,
+        end_date TIMESTAMP,
+        rental_cost DECIMAL(10, 2)
+        );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE bookings;`);
+  }
+}

c93751cbc53002e6ca899e2d8d19a169d8cf8933
Daniil Druzhina <danil.druzhina@gmail.com>
2023-08-23 13:37:46 +0300

    add dto validation

--- src/bookings/dto/create-booking.dto.ts
+++ src/bookings/dto/create-booking.dto.ts
@@ -1,5 +1,15 @@
+import { IsDateString, IsInt, IsNotEmpty } from 'class-validator';
+
 export class CreateBookingDto {
+  @IsInt()
+  @IsNotEmpty()
   carId: number;
+
+  @IsDateString()
+  @IsNotEmpty()
   startDate: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   endDate: string;
 }
